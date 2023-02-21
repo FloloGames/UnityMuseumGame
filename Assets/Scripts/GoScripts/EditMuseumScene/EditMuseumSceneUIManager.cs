23ab64f0f1f2708ae620c90234551acfff7de999
Florian Wentzel <flolow2005@gmail.com>
2023-02-21 23:37:47 +0100

    Updated not working

--- Assets/Scripts/GoScripts/EditMuseumScene/EditMuseumSceneUIManager.cs
+++ Assets/Scripts/GoScripts/EditMuseumScene/EditMuseumSceneUIManager.cs
@@ -1,23 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class EditMuseumSceneUIManager : MonoBehaviour
 {
-    public class PanelContainerManager
-    {
-        public int CurrSelectedPanel = 0;
-        public List<PanelContainer> Panels;
-        public void SetEveryPanelToClosedSkip()
-        {
-            foreach (var panel in Panels)
-            {
-                panel.CloseSkip();
-            }
-        }
-    }
     public class PanelContainer
     {
         public string name;
@@ -26,16 +15,16 @@ public class EditMuseumSceneUIManager : MonoBehaviour
         public float ClosedPos;
 
         /*
-         public async void MyMethod()
-        {
             await LeanTween.moveX(gameObject, 5f, 1f).setEaseOutQuad().setOnComplete(() => {
                 Debug.Log("Tween completed.");
             });
-
             Debug.Log("Next set of instructions.");
-        }
-         */
-        public void CloseSkip(System.Action OnComplete = null)
+        */
+        /// <summary>
+        /// Closes Panel with or without animation
+        /// </summary>
+        /// <param name="OnComplete">if not given skip otherwise with animation</param>
+        public void Close(Action OnComplete = null)
         {
             if (OnComplete == null)
             {
@@ -46,108 +35,140 @@ public class EditMuseumSceneUIManager : MonoBehaviour
                 float time = 0.5f;
                 PanelTransform.LeanMoveY(ClosedPos, time).setEaseInBack().setOnComplete(OnComplete);
             }
-
-
         }
-        public void CloseAnimation(System.Action OnComplete)
+        public void Open(Action OnComplete = null)
         {
-
-        }
-        public void OpenSkip()
-        {
-            PanelTransform.anchoredPosition = new Vector3(0, OpenPos, 0);
-        }
-        public void OpenAnimation(System.Action OnComplete)
-        {
-            float time = 0.5f;
-            PanelTransform.LeanMoveY(OpenPos, time).setEaseOutBack().setOnComplete(OnComplete);
-        }
-    }
-    public static string ACTION_PANEL_NAME = "ACTION";
-    public static string TOP_PANEL_NAME = "TOP";
-    public static string BOTTOM_PANEL_NAME = "BOTTOMM";
-
-    private static EditMuseumSceneUIManager _instance;
-    public static EditMuseumSceneUIManager Instance => _instance;
-
-    private List<int> SelectedPanelIndices;
-
-    [SerializeField]
-    private PanelContainerManager TopPanels;
-    [SerializeField]
-    private PanelContainerManager BottomPanels;
-
-
-    private void Awake()
-    {
-        _instance = this;
-        SetEveryPanelToClosed();
-        SetEveryPanelObjectActive();
-        OpenPanel("");
-    }
-
-    private void SetEveryPanelObjectActive()
-    {
-        foreach (var panel in Panels)
-        {
-            panel.PanelTransform.gameObject.SetActive(true);
-        }
-    }
-    private void ClosePanelSkip(int index)
-    {
-        if (index >= Panels.Count)
-        {
-            Debug.LogError("Index Out of Bounce! " + index);
-            return;
-        }
-        Panels[index].CloseSkip();
-    }
-    private void ClosePanelAnim<T>(int index, System.Action<T> OnComplete, T value)
-    {
-        if (index >= Panels.Count)
-        {
-            Debug.LogError("Index Out of Bounce! " + index);
-            return;
-        }
-        Panels[index].CloseAnimation(() => { OnComplete(value); });
-    }
-    private int GetPanelIndexByName(string panelName)
-    {
-        for (int i = 0; i < Panels.Count; i++)
-        {
-            var panel = Panels[i];
-            if (panel.name == panelName)
+            if (OnComplete == null)
             {
-                return i;
+                PanelTransform.anchoredPosition = new Vector3(0, OpenPos, 0);
+            }
+            else
+            {
+                float time = 0.5f;
+                PanelTransform.LeanMoveY(OpenPos, time).setEaseOutBack().setOnComplete(OnComplete);
             }
         }
-        return -1;
     }
-    public void OpenPanelsAnim(int[] indices)
+    public class PanelContainerManager
     {
-        for (int i = 0; i < indices.Length; i++)
+        public int CurrSelectedPanel = 0;
+        public List<PanelContainer> Panels;
+        public void SetEveryPanelToClosedSkip()
         {
-            int index = indices[i];
+            foreach (var panel in Panels)
+            {
+                panel.Close();
+            }
+        }
+        public bool ContainsPanel(string panelName)
+        {
+            return GetPanelIndexByName(panelName) != -1;
+        }
+        public void SetEveryPanelObjectActive(bool value)
+        {
+            foreach (var panel in Panels)
+            {
+                panel.PanelTransform.gameObject.SetActive(value);
+            }
+        }
+        public async Task CloseCurrAndOpenNewPanelAsync(string panelName)
+        {
+            int index = GetPanelIndexByName(panelName);
+            if (index == -1)
+                return;
+            await ClosePanel(CurrSelectedPanel, () => { });
+        }
+        public void ClosePanel(string panelName, Action OnComplete = null)
+        {
+            int index = GetPanelIndexByName(panelName);
+            if (index == -1)
+                return;
+            ClosePanel(index, OnComplete);
+        }
+        public async Task ClosePanel(int index, Action OnComplete = null)
+        {
             if (index >= Panels.Count)
             {
                 Debug.LogError("Index Out of Bounce! " + index);
                 return;
             }
-            Panels[index].OpenAnimation(null);
+            if (OnComplete == null)
+            {
+                Panels[index].Close();
+            }
+            else
+            {
+                Panels[index].Close(OnComplete);
+            }
+        }
+        public void OpenPanel(string panelName, Action OnComplete = null)
+        {
+            int index = GetPanelIndexByName(panelName);
+            if (index == -1)
+                return;
+            OpenPanel(index, OnComplete);
+        }
+        public void OpenPanel(int index, Action OnComplete = null)
+        {
+            if (index >= Panels.Count)
+            {
+                Debug.LogError("Index Out of Bounce! " + index);
+                return;
+            }
+            if (OnComplete == null)
+                Panels[index].Open();
+            else
+                Panels[index].Open(OnComplete);
+
+        }
+        private int GetPanelIndexByName(string panelName)
+        {
+            for (int i = 0; i < Panels.Count; i++)
+            {
+                var panel = Panels[i];
+                if (panel.name == panelName)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
-    public void OpenPanelAnim(int index, System.Action OnComplete)
+
+    public static string ACTION_PANEL_NAME = "ACTION";
+    public static string TOP_PANEL_NAME = "TOP";
+    public static string BOTTOM_PANEL_NAME = "BOTTOM";
+
+    private static EditMuseumSceneUIManager _instance;
+    public static EditMuseumSceneUIManager Instance => _instance;
+
+    [SerializeField]
+    private PanelContainerManager TopPanelsManager;
+    [SerializeField]
+    private PanelContainerManager BottomPanelsManager;
+
+
+    private void Awake()
     {
-        if (index >= Panels.Count)
-        {
-            Debug.LogError("Index Out of Bounce! " + index);
-            return;
-        }
-        Panels[index].OpenAnimation(OnComplete);
+        _instance = this;
+        TopPanelsManager.SetEveryPanelToClosedSkip();
+        BottomPanelsManager.SetEveryPanelToClosedSkip();
+
+        TopPanelsManager.SetEveryPanelObjectActive(true);
+        BottomPanelsManager.SetEveryPanelObjectActive(true);
+        OpenPanel("");
     }
     public void OpenPanel(string panelName)
     {
-        OpenPanels(new[] { panelName });
+        if (TopPanelsManager.ContainsPanel(panelName))
+        {
+            TopPanelsManager.OpenPanel(ACTION_PANEL_NAME, );
+            return;
+        }
+        if (BottomPanelsManager.ContainsPanel(panelName))
+        {
+
+        }
     }
     public void OpenPanels(params string[] panelNames)
     {
